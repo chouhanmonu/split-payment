fb13c0a550036e6616ed90e557d8dd28ddecd4d2
Monu Chouhan <chouhan.monu07@gmail.com>
2025-10-22 18:37:54 +0530

    add app start log

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { isProduction } from './utility/env.util';
 
 @Injectable()
 export class AppService {
-  constructor() {
-    console.log({
-      production: isProduction(),
-    });
-  }
-
   getHello(): string {
     return 'Hello World!';
   }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getEnvironment } from './utility/env.util';
 
 async function bootstrap() {
+  console.log(`App running in \x1b[34m${getEnvironment()}\x1b[0m environment`);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
